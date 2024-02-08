86a539e5c65e233d74d4f48e9eb443c6836fc46d
Tobias Bechtold <tobias.bechtold@atruvia.de>
2024-02-08 16:50:55 +0100

    fix cypress tests

--- cypress/AppComponent.cy.ts
+++ cypress/AppComponent.cy.ts
@@ -1,36 +1,45 @@
 import {AppComponent} from "../src/app/app.component";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { BundesligaTableService } from "../src/app/services/bundesliga-table.service";
+import { Observable, of } from "rxjs";
+import { Team } from "../src/app/models/team.ui.model";
+
+let tableService = {
+  getTableFromServer(liga: string, saison: string): Observable<Team[]> {
+    return of([{
+      "platz": 1,
+      "wappen": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Borussia_Dortmund_logo.svg/560px-Borussia_Dortmund_logo.svg.png",
+      "team": "Borussia Dortmund",
+      "spiele": 25,
+      "punkte": 53,
+      "tore": 55,
+      "gegentore": 31,
+      "tordifferenz": 24,
+      "siege": 17,
+      "unentschieden": 2,
+      "niederlagen": 6,
+      "letzte5": [
+        "../../assets/sieg.svg",
+        "../../assets/niederlage.svg",
+        "../../assets/niederlage.svg",
+        "../../assets/niederlage.svg",
+        "../../assets/niederlage.svg"
+      ]
+    }]);
+  }
+};
 
 describe('AppComponent.cy.ts', () => {
   it('mounts', () => {
-    cy.mount(AppComponent);
+    cy.mount(AppComponent, {
+      imports: [HttpClientTestingModule]
+    });
   });
 
   it('displays one team', () => {
     cy.mount(AppComponent, {
-      componentProperties: {
-        table: [
-          {
-            "platz": 1,
-            "wappen": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Borussia_Dortmund_logo.svg/560px-Borussia_Dortmund_logo.svg.png",
-            "team": "Borussia Dortmund",
-            "spiele": 25,
-            "punkte": 53,
-            "tore": 55,
-            "gegentore": 31,
-            "tordifferenz": 24,
-            "siege": 17,
-            "unentschieden": 2,
-            "niederlagen": 6,
-            "letzte5": [
-              "../../assets/sieg.svg",
-              "../../assets/niederlage.svg",
-              "../../assets/niederlage.svg",
-              "../../assets/niederlage.svg",
-              "../../assets/niederlage.svg"
-            ]
-          }
-        ]
-      }
+      imports: [HttpClientTestingModule],
+      providers: [{provide: BundesligaTableService, useValue: tableService}],
     });
     cy.get('[data-cy="teamname"]').should('have.text', 'Borussia Dortmund');
   });
