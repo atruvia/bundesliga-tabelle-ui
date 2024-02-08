d0a731ae06d3a13a3eef732c58f9db7b0eb41ed2
Tobias Bechtold <tobias.bechtold@atruvia.de>
2024-02-08 17:06:00 +0100

    show error if backend is not available

--- cypress/AppComponent.cy.ts
+++ cypress/AppComponent.cy.ts
@@ -1,7 +1,7 @@
 import {AppComponent} from "../src/app/app.component";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { BundesligaTableService } from "../src/app/services/bundesliga-table.service";
-import { Observable, of } from "rxjs";
+import { Observable, of, throwError } from "rxjs";
 import { Team } from "../src/app/models/team.ui.model";
 
 let tableService = {
@@ -29,6 +29,12 @@ let tableService = {
   }
 };
 
+let errorTableService = {
+  getTableFromServer(liga: string, saison: string): Observable<Team[]> {
+    return throwError('error');
+  }
+};
+
 describe('AppComponent.cy.ts', () => {
   it('mounts', () => {
     cy.mount(AppComponent, {
@@ -42,5 +48,13 @@ describe('AppComponent.cy.ts', () => {
       providers: [{provide: BundesligaTableService, useValue: tableService}],
     });
     cy.get('[data-cy="teamname"]').should('have.text', 'Borussia Dortmund');
+  });
+
+  it('shows error when backend is not available', () => {
+    cy.mount(AppComponent, {
+      imports: [HttpClientTestingModule],
+      providers: [{provide: BundesligaTableService, useValue: errorTableService}],
+    });
+    cy.get('[data-cy="error"]').should('have.text', 'Fehler beim Laden der Tabelle');
   });
 });
