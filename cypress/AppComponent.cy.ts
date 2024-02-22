9c4000a057a723130316f7b035678dcb7a859188
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-02-22 15:46:01 +0100

    test most of the mapping

--- cypress/AppComponent.cy.ts
+++ cypress/AppComponent.cy.ts
@@ -7,17 +7,17 @@ import { Team } from "../src/app/models/team.ui.model";
 let tableService = {
   getTableFromServer(liga: string, saison: string): Observable<Team[]> {
     return of([{
-      "platz": 1,
-      "wappen": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Borussia_Dortmund_logo.svg/560px-Borussia_Dortmund_logo.svg.png",
-      "team": "Borussia Dortmund",
-      "spiele": 25,
-      "punkte": 53,
-      "tore": 55,
-      "gegentore": 31,
-      "tordifferenz": 24,
-      "siege": 17,
-      "unentschieden": 2,
-      "niederlagen": 6,
+      "platz": 12,
+      "wappen": "https://i.imgur.com/jJEsJrj.png",
+      "team": "Ein recht langer Teamname",
+      "spiele": 23,
+      "punkte": 34,
+      "tore": 45,
+      "gegentore": 56,
+      "tordifferenz": 67,
+      "siege": 78,
+      "unentschieden": 89,
+      "niederlagen": 90,
       "letzte5": [
         "../../assets/sieg.svg",
         "../../assets/niederlage.svg",
@@ -47,7 +47,18 @@ describe('AppComponent.cy.ts', () => {
       imports: [HttpClientTestingModule],
       providers: [{provide: BundesligaTableService, useValue: tableService}],
     });
-    cy.get('[data-cy="teamname"]').should('have.text', 'Borussia Dortmund');
+    cy.get('[data-cy="platz"]').should('have.text', '12.'); // <-- dot gets appended :)
+    cy.get('[data-cy="wappen"]').should('have.attr', 'src', 'https://i.imgur.com/jJEsJrj.png');
+    cy.get('[data-cy="teamname"]').should('have.text', 'Ein recht langer Teamname');
+    cy.get('[data-cy="spiele"]').should('have.text', '23');
+    cy.get('[data-cy="punkte"]').should('have.text', '34');
+    cy.get('[data-cy="tore"]').should('have.text', '45');
+    cy.get('[data-cy="gegentore"]').should('have.text', '56');
+    cy.get('[data-cy="tordifferenz"]').should('have.text', '67');
+    cy.get('[data-cy="siege"]').should('have.text', '78');
+    cy.get('[data-cy="unentschieden"]').should('have.text', '89');
+    cy.get('[data-cy="niederlagen"]').should('have.text', '90');
+    // cy.get('[data-cy="letzte5"]').should('have.text', '');
   });
 
   it('shows error when backend is not available', () => {
