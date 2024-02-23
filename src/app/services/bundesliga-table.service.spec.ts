1465e176820cb39fd1bc632ddefa03f7bf52819d
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-02-23 09:34:07 +0100

    removed duplications, skipping test that test the whole mapping

--- src/app/services/bundesliga-table.service.spec.ts
+++ src/app/services/bundesliga-table.service.spec.ts
@@ -13,29 +13,30 @@ describe('BundesligaTableService', () => {
     service = TestBed.inject(BundesligaTableService);
   });
 
-  it('should be created', () => {
+  const backendDataMother = [
+    {
+      "platz": 12,
+      "wappen": "wappen",
+      "team": "Team",
+      "spiele": 23,
+      "punkte": 34,
+      "tore": 45,
+      "gegentore": 56,
+      "tordifferenz": 67,
+      "siege": 78,
+      "unentschieden": 89,
+      "niederlagen": 90,
+      "letzte5": "SNNUN"
+    }
+  ];
+
+    it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should map backend model to frontend model', ()=> {
-    const testDataFromBackend = [
-      {
-        "platz": 12,
-        "wappen": "wappen",
-        "team": "Team",
-        "spiele": 23,
-        "punkte": 34,
-        "tore": 45,
-        "gegentore": 56,
-        "tordifferenz": 67,
-        "siege": 78,
-        "unentschieden": 89,
-        "niederlagen": 90,
-        "letzte5": "SNNNN"
-      }
-    ];
-
-    expect(service.transformToUIModel(testDataFromBackend)).toEqual([{
+  // mapping the whole model is tested using the pact test
+  it.skip('should map backend model to frontend model', ()=> {
+    expect(service.transformToUIModel(backendDataMother)).toEqual([{
       "platz": 12,
       "wappen": "wappen",
       "team": "Team",
@@ -51,90 +52,34 @@ describe('BundesligaTableService', () => {
         "../../assets/sieg.svg",
         "../../assets/niederlage.svg",
         "../../assets/niederlage.svg",
-        "../../assets/niederlage.svg",
+        "../../assets/unentschieden.svg",
         "../../assets/niederlage.svg"
       ]
     }]);
   });
 
   it('should fill up right if less than 5 games were played', ()=> {
-    const testDataFromBackend = [
-      {
-        "platz": 12,
-        "wappen": "wappen",
-        "team": "Team",
-        "spiele": 23,
-        "punkte": 34,
-        "tore": 45,
-        "gegentore": 56,
-        "tordifferenz": 67,
-        "siege": 78,
-        "unentschieden": 89,
-        "niederlagen": 90,
-        "letzte5": "SUN"
-      }
-    ];
+    backendDataMother[0].letzte5 = "SUN"
 
-    expect(service.transformToUIModel(testDataFromBackend)).toEqual([{
-      "platz": 12,
-      "wappen": "wappen",
-      "team": "Team",
-      "spiele": 23,
-      "punkte": 34,
-      "tore": 45,
-      "gegentore": 56,
-      "tordifferenz": 67,
-      "siege": 78,
-      "unentschieden": 89,
-      "niederlagen": 90,
-      "letzte5": [
+    expect(service.transformToUIModel(backendDataMother)[0].letzte5).toEqual([
         "../../assets/sieg.svg",
         "../../assets/unentschieden.svg",
         "../../assets/niederlage.svg",
         "../../assets/nicht-gespielt.svg",
         "../../assets/nicht-gespielt.svg",
-      ]
-    }]);
+    ]);
   });
 
   it('any char not SUN gets interprted as not played', ()=> {
-    const testDataFromBackend = [
-      {
-        "platz": 12,
-        "wappen": "wappen",
-        "team": "Team",
-        "spiele": 23,
-        "punkte": 34,
-        "tore": 45,
-        "gegentore": 56,
-        "tordifferenz": 67,
-        "siege": 78,
-        "unentschieden": 89,
-        "niederlagen": 90,
-        "letzte5": "SX N-"
-      }
-    ];
+    backendDataMother[0].letzte5 = "SX N-"
 
-    expect(service.transformToUIModel(testDataFromBackend)).toEqual([{
-      "platz": 12,
-      "wappen": "wappen",
-      "team": "Team",
-      "spiele": 23,
-      "punkte": 34,
-      "tore": 45,
-      "gegentore": 56,
-      "tordifferenz": 67,
-      "siege": 78,
-      "unentschieden": 89,
-      "niederlagen": 90,
-      "letzte5": [
+    expect(service.transformToUIModel(backendDataMother)[0].letzte5).toEqual([
         "../../assets/sieg.svg",
         "../../assets/nicht-gespielt.svg",
         "../../assets/nicht-gespielt.svg",
         "../../assets/niederlage.svg",
         "../../assets/nicht-gespielt.svg",
-      ]
-    }]);
+    ]);
   });
 
 
